eef28ffab70f8664c67f2de466859f3f56d13a78
MarcioCruzTR <Marcio.Cruz@thomsonreuters.com>
2025-11-06 22:10:08 -0300

    Refatorando estrutura do projeto: Criado routers para Users e Auth; movido constantes para variáveis de ambiente.

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,6 @@ from http import HTTPStatus
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
-from fast_zero.schemas import UserPublic
 
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
@@ -15,56 +14,6 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_create_user(client):
-    response = client.post(
-        '/users',
-        json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
-        },
-    )
-    assert response.status_code == HTTPStatus.CREATED
-    assert response.json() == {
-        'username': 'alice',
-        'id': 1,
-        'email': 'alice@example.com',
-    }
-
-
-def test_read_users(client):
-    response = client.get('/users')
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'users': []}
-
-
-def test_update_user(client, user, token):
-    response = client.put(
-        f'/users/{user.id}',
-        headers={'Authorization': f'Bearer {token}'},
-        json={
-            'username': 'bob',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
-        },
-    )
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': user.id,
-    }
-
-
-def test_delete_user(client, user, token):
-    response = client.delete(
-        f'/users/{user.id}',
-        headers={'Authorization': f'Bearer {token}'},
-    )
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'User deleted'}
-
-
 def test_exercicio_ola_mundo_em_html():
     client = TestClient(app)
 
@@ -74,15 +23,9 @@ def test_exercicio_ola_mundo_em_html():
     assert '<h1> Olá Mundo </h1>' in response.text
 
 
-def test_read_users_with_users(client, user):
-    user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get('/users/')
-    assert response.json() == {'users': [user_schema]}
-
-
 def test_get_token(client, user):
     response = client.post(
-        '/token',
+        '/auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     token = response.json()
