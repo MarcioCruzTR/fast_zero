695264af8cfb962668067a5af36af0652cf946cd
MarcioCruzTR <Marcio.Cruz@thomsonreuters.com>
2025-11-06 22:49:51 -0300

    Refatorando estrutura do projeto: Suporte a asyncio, tornando o projeto não bloqueante

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,7 @@
+import asyncio
+
 from logging.config import fileConfig
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -28,6 +31,29 @@ target_metadata = table_registry.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 
+def do_run_migrations(connection): 
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations(): 
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online(): 
+    asyncio.run(run_async_migrations())
+    
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -50,29 +76,6 @@ def run_migrations_offline() -> None:
 
     with context.begin_transaction():
         context.run_migrations()
-
-
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
